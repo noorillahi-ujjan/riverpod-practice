8108415fa8725d10f1536cec7ac857ac8021f5eb
noorillahi-ujjan <noorillahi@fastechsystem.co>
2023-11-02 00:30:53 +0500

    ConsumerStatefullWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,71 @@ import 'package:flutter/material.dart';
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import 'package:riverpod_practice/second_screen.dart';
 
+// final nameProvider = Provider<String>((ref) => "Noor ");
+
+// void main() {
+//   runApp(const ProviderScope(child: MyApp()));
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(
+//     BuildContext context,
+//   ) {
+//     // final name = ref.watch(nameProvider);
+//     return MaterialApp(
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home:
+//           // SecondScreen(),
+//           Consumer(builder: (context, ref, child) {
+//         final name = ref.watch(nameProvider);
+//         return Center(
+//           child: Container(
+//             child: Text("Main Screen $name"),
+//           ),
+//         );
+//       }),
+//     );
+//   }
+// }
+
+//************* if the widget is StatefullWidget */
+
 final nameProvider = Provider<String>((ref) => "Noor ");
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
+  @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
   @override
   Widget build(
     BuildContext context,
   ) {
-    // final name = ref.watch(nameProvider);
+    final name = ref.watch(nameProvider);
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:
-          // SecondScreen(),
-          Consumer(builder: (context, ref, child) {
-        final name = ref.watch(nameProvider);
-        return Center(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home:
+            // SecondScreen(),
+            Center(
           child: Container(
             child: Text("Main Screen $name"),
           ),
-        );
-      }),
-    );
+        ));
   }
 }
